b4423475ca03802fcfe62a0cbd02c9085e521d01
Yonghyu Ban <yhban@cleanc.kr>
2023-09-12 03:35:14 +0900

    Fix missing socat

--- jenkins-agent/entrypoint.sh
+++ jenkins-agent/entrypoint.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 export DOCKER_HOST=unix:///var/run/docker.sock
 
-# Test docker.sock using socat
-socat -u OPEN:/dev/null UNIX-CONNECT:/var/run/docker.sock
+LAUNCH_DIND=false
 
-if [ "$?" != '0' ]; then
-    echo "Docker socket is not forwarded. Spawning docker..."
+if [ ! -e /var/run/docker.sock ]; then
+    echo 'Docker socket is not forwarded.'
+    LAUNCH_DIND=true
+else
+    # Test docker.sock using socat
+    socat -u OPEN:/dev/null UNIX-CONNECT:/var/run/docker.sock
+    if [ "$?" != '0' ]; then
+        echo 'Docker socket exists but not responding. Maybe container restart?'
+        LAUNCH_DIND=true
+    fi
+fi
+
+if [ "$LAUNCH_DIND" == 'true' ]; then
+    echo "Cannot communicate with docker. Launching Docker in Docker..."
     /usr/local/bin/dockerd-entrypoint.sh dockerd &
 else
-    echo "Forwarded docker socket detected. Using host docker container..."
+    echo "Can communicate with docker. Using given docker socket."
 fi
 
 exec /usr/local/bin/jenkins-agent
